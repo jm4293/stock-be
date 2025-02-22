e4d686056a855a5b6776fd67fa61c2b915c6c446
jm4293 <dlwoals4293@gmail.com>
2025-02-23 03:59:07 +0900

    [Chore]: CORS PORT 변경

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,9 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:31180'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    // credentials: true,
+    credentials: true,
   });
 
   app.useGlobalPipes(ValidationPipeConfig);
