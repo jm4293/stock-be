b1999c5df00987c92840361ca59f6ffaaa8e387d
jm4293 <dlwoals4293@gmail.com>
2025-02-27 01:19:09 +0900

    [Refactor]: dataSource: DataSource 제거

--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -9,7 +9,6 @@ import {
   CreateUserEmailDto,
   LoginOauthDto,
 } from '../../type/interface';
-import { DataSource } from 'typeorm';
 import { BcryptHandler } from '../../handler';
 import { User, UserAccount } from 'src/database/entities';
 import { UserAccountTypeEnum, UserVisitTypeEnum } from '../../type/enum';
@@ -31,49 +30,31 @@ export class AuthService {
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
     private readonly httpService: HttpService,
-
-    private readonly dataSource: DataSource,
   ) {}
 
   async registerEmail(dto: CreateUserEmailDto): Promise<IPostCreateUserEmailRes> {
-    const queryRunner = this.dataSource.createQueryRunner();
-    await queryRunner.connect();
-    await queryRunner.startTransaction();
+    const { nickname, name, policy, birthdate, email, password } = dto;
 
-    try {
-      const { nickname, name, policy, birthdate, email, password } = dto;
+    let user: User;
 
-      let user: User;
+    const isUserAccountExist = await this.userAccountRepository.findUserAccountByEmail(email);
 
-      const isUserAccountExist = await this.userAccountRepository.findUserAccountByEmail(email);
-
-      if (isUserAccountExist) {
-        user = isUserAccountExist.user;
-      } else {
-        user = await this.userRepository.createUser({ nickname, name, policy, birthdate });
-      }
-
-      const hashedPassword = await BcryptHandler.hashPassword(password);
-
-      const userAccount = await this.userAccountRepository.createUserAccountByEmail({
-        type: UserAccountTypeEnum.EMAIL,
-        email,
-        password: hashedPassword,
-        user,
-      });
-
-      await queryRunner.manager.save(user);
-      await queryRunner.manager.save(userAccount);
-
-      await queryRunner.commitTransaction();
-
-      return { email: userAccount.email };
-    } catch (err) {
-      await queryRunner.rollbackTransaction();
-      return err;
-    } finally {
-      await queryRunner.release();
+    if (isUserAccountExist) {
+      user = isUserAccountExist.user;
+    } else {
+      user = await this.userRepository.createUser({ nickname, name, policy, birthdate });
     }
+
+    const hashedPassword = await BcryptHandler.hashPassword(password);
+
+    const userAccount = await this.userAccountRepository.createUserAccountByEmail({
+      type: UserAccountTypeEnum.EMAIL,
+      email,
+      password: hashedPassword,
+      user,
+    });
+
+    return { email: userAccount.email };
   }
 
   async checkEmail(dto: CheckEmailDto): Promise<IPostCheckEmailRes> {
