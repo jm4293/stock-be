54176234000711f5f55bbc49909d055ca5286767
jm4293 <dlwoals4293@gmail.com>
2025-03-12 17:30:45 +0900

    [Refactor]: 주식 코드 리스트 조회, 주식 코드 상세 조회 API

--- src/module/kis/kis.controller.ts
+++ src/module/kis/kis.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Delete, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, Query, Req, Res } from '@nestjs/common';
 import { KisService } from './kis.service';
 import { ResConfig } from '../../config';
 import { Request, Response } from 'express';
+import { Public } from '../../decorator';
 
 @Controller('kis')
 export class KisController {
   constructor(private readonly kisService: KisService) {}
 
+  // 토큰
   @Get('oauth-token')
   async getOuathToken(@Req() req: Request, @Res() res: Response) {
     const ret = await this.kisService.getOuathToken({ req });
@@ -14,9 +16,20 @@ export class KisController {
     return ResConfig.Success({ res, statusCode: 'OK', data: ret });
   }
 
-  @Delete('oauth-revoke')
-  async deleteOuathToken(@Res() res: Response) {
-    const ret = await this.kisService.deleteOuathToken();
+  // 종목 코드 조회
+  @Public()
+  @Get('code-list')
+  async getCodeList(@Res() res: Response) {
+    const ret = await this.kisService.getCodeList();
+
+    return ResConfig.Success({ res, statusCode: 'OK', data: ret });
+  }
+
+  // 종목 상세 조회
+  @Public()
+  @Get('code-detail')
+  async getCodeDetail(@Query('code') code: string, @Res() res: Response) {
+    const ret = await this.kisService.getCodeDetail({ code });
 
     return ResConfig.Success({ res, statusCode: 'OK', data: ret });
   }
--- src/module/kis/kis.service.ts
+++ src/module/kis/kis.service.ts
@@ -1,14 +1,19 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpService } from '@nestjs/axios';
-import { catchError, firstValueFrom, map } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { KisTokenIssueRepository, KisTokenRepository, UserRepository } from '../../database/repository';
 import { IKisCreateToken } from '../../type/interface';
 import { Request } from 'express';
+import { ResConfig } from '../../config';
+import { KisToken } from '../../database/entities';
+import { AxiosResponse } from 'axios';
 
 @Injectable()
 export class KisService implements OnModuleInit {
+  private kisToken: KisToken | null = null;
+
   constructor(
     private readonly configService: ConfigService,
     private readonly httpService: HttpService,
@@ -19,6 +24,7 @@ export class KisService implements OnModuleInit {
   ) {}
 
   async onModuleInit() {
+    // await this._deleteKisToken();
     // await this.handleCron();
   }
 
@@ -28,45 +34,38 @@ export class KisService implements OnModuleInit {
 
     console.log(`[${now}] Scheduler 'kis Token' called`);
 
-    const httpRet = this.httpService
-      .post(`${this.configService.get('KIS_APP_URL')}/oauth2/tokenP`, {
+    const ret = await firstValueFrom<AxiosResponse<IKisCreateToken>>(
+      this.httpService.post(`${this.configService.get('KIS_APP_URL')}/oauth2/tokenP`, {
         grant_type: 'client_credentials',
         appkey: this.configService.get('KIS_APP_KEY'),
         appsecret: this.configService.get('KIS_APP_SECRET'),
-      })
-      .pipe(
-        map((response) => {
-          return response.data;
-        }),
-        catchError((error) => {
-          console.error('Error occurred while fetching OAuth token:', error);
-          throw new Error('Failed to fetch OAuth token');
-        }),
-      );
+      }),
+    );
 
-    const ret: IKisCreateToken = await firstValueFrom(httpRet);
+    const { access_token, access_token_token_expired, token_type, expires_in } = ret.data;
 
-    const existingToken = await this.kisTokenRepository.findOne({ where: { kisTokenSeq: 1 } });
+    const isKisToken = await this.kisTokenRepository.findOne({ where: { kisTokenSeq: 1 } });
 
-    if (existingToken) {
-      existingToken.accessToken = ret.access_token;
-      existingToken.accessTokenExpired = ret.access_token_token_expired;
-      existingToken.tokenType = ret.token_type;
-      existingToken.expiresIn = ret.expires_in;
+    if (isKisToken) {
+      isKisToken.accessToken = access_token;
+      isKisToken.accessTokenExpired = access_token_token_expired;
+      isKisToken.tokenType = token_type;
+      isKisToken.expiresIn = expires_in;
 
-      await this.kisTokenRepository.save(existingToken);
+      await this.kisTokenRepository.save(isKisToken);
     } else {
       const token = this.kisTokenRepository.create({
-        accessToken: ret.access_token,
-        accessTokenExpired: ret.access_token_token_expired,
-        tokenType: ret.token_type,
-        expiresIn: ret.expires_in,
+        accessToken: access_token,
+        accessTokenExpired: access_token_token_expired,
+        tokenType: token_type,
+        expiresIn: expires_in,
       });
 
       await this.kisTokenRepository.save(token);
     }
   }
 
+  // 토큰
   async getOuathToken(params: { req: Request }) {
     const { req } = params;
     const { headers, ip = null, user } = req;
@@ -75,32 +74,105 @@ export class KisService implements OnModuleInit {
 
     await this.userRepository.findUserByUserSeq(userSeq);
 
-    const kisToken = await this.kisTokenRepository.find();
+    const kisToken = await this._getKisToken();
 
-    await this.kisTokenIssueRepository.save({ userSeq, kisTokenSeq: kisToken[0].kisTokenSeq, ip, userAgent, referer });
+    await this.kisTokenIssueRepository.save({ userSeq, kisTokenSeq: kisToken.kisTokenSeq, ip, userAgent, referer });
 
-    return { kisToken: kisToken[0].accessToken };
+    return { kisToken: kisToken.accessToken };
   }
 
-  async deleteOuathToken() {
-    //   const ret = this.httpService
-    //     .post(`${this.configService.get('KIS_APP_URL')}/oauth2/revokeP`, {
-    //       token: this.oauthToken,
+  async getCodeList() {
+    // const kisToken = await this._getKisToken();
+    //
+    // const ret = await firstValueFrom(
+    //   this.httpService.get(`${this.configService.get('KIS_APP_URL')}/uapi/domestic-stock/v1/quotations/inquire-ccnl`, {
+    //     headers: {
+    //       'Content-Type': 'application/json; charset=utf-8',
+    //       Authorization: `Bearer ${kisToken.accessToken}`,
     //       appkey: this.configService.get('KIS_APP_KEY'),
     //       appsecret: this.configService.get('KIS_APP_SECRET'),
-    //     })
-    //     .pipe(
-    //       map((response) => {
-    //         this.oauthToken = null;
+    //       tr_id: 'FHKST01010100',
+    //       custtype: 'P',
+    //     },
+    //     params: {
+    //       FID_COND_MRKT_DIV_CODE: 'J', // 시장 구분 코드
+    //     },
+    //   }),
+    // );
     //
-    //         return response.data;
-    //       }),
-    //       catchError((error) => {
-    //         console.error('Error occurred while deleting OAuth token:', error);
-    //         throw new Error('Failed to delete OAuth token');
-    //       }),
-    //     );
+    // if (ret.status !== 200) {
+    //   throw ResConfig.Fail_400({ message: '종목 리스트 조회에 실패하였습니다.' });
+    // }
     //
-    //   return await firstValueFrom<boolean>(ret);
+    // return ret.data;
+
+    const ret: any = await firstValueFrom(
+      this.httpService.get('http://api.seibro.or.kr/openapi/service/StockSvc/getKDRSecnInfo', {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        params: {
+          serviceKey: `${this.configService.get('DATA_GO_KR_STOCK_CODE_SERVICE_KEY')}`,
+          caltotMartTpcd: '12',
+        },
+      }),
+    );
+
+    return ret.data;
+  }
+
+  async getCodeDetail(params: { code: string }) {
+    const { code } = params;
+
+    const kisToken = await this._getKisToken();
+
+    const ret = await firstValueFrom(
+      this.httpService.get(`${this.configService.get('KIS_APP_URL')}/uapi/domestic-stock/v1/quotations/inquire-price`, {
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          Authorization: `Bearer ${kisToken.accessToken}`,
+          appkey: this.configService.get('KIS_APP_KEY'),
+          appsecret: this.configService.get('KIS_APP_SECRET'),
+          tr_id: 'FHKST01010100',
+          custtype: 'P',
+        },
+        params: {
+          FID_COND_MRKT_DIV_CODE: 'J',
+          FID_INPUT_ISCD: code,
+        },
+      }),
+    );
+
+    if (ret.status !== 200) {
+      throw ResConfig.Fail_400({ message: '종목 조회에 실패하였습니다.' });
+    }
+
+    return ret.data;
+  }
+
+  private async _getKisToken() {
+    if (!this.kisToken) {
+      const kisToken = await this.kisTokenRepository.find();
+
+      if (!kisToken || !kisToken[0]) {
+        console.error('Kis token이 DB에 존재하지 않습니다.');
+      }
+
+      this.kisToken = kisToken[0];
+    }
+
+    return this.kisToken;
+  }
+
+  private async _deleteKisToken() {
+    if (this.kisToken) {
+      return await firstValueFrom(
+        this.httpService.post(`${this.configService.get('KIS_APP_URL')}/oauth2/revokeP`, {
+          token: this.kisToken.accessToken,
+          appkey: this.configService.get('KIS_APP_KEY'),
+          appsecret: this.configService.get('KIS_APP_SECRET'),
+        }),
+      );
+    }
   }
 }
